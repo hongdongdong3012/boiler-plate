63b3b7ce547277b460c428fc1427a5c1476c4864
jinwoo <hongdongdong3012@gmail.com>
2021-01-22 22:51:10 +0900

    서버 완료!

--- index.js
+++ index.js
@@ -1,39 +1,96 @@
+//express와 연결
 const express = require('express')
 const app = express()
 const port = 5000
-const bodyParser = require('body-parser')
-
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key')
-
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json())
+app.use(cookieParser())
 
+//mongoose와 연결
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
+  //이거 써야 애러가 안뜬다.
   useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+}).then(() => console.log('MongoDB Connected...')) //연결이 잘 됬는지 확인
+  .catch(err => console.log(err)) //연결이 잘 안됬을때 err
 
 
 app.get('/', (req, res) => res.send('난 진우야'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   //회원가입할때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
 
-
-  const user = new User(req.body)
-
-  user.save((err, userInfo) => {
+  const user = new User(req.body) //모든 정보를 model에 넣음.
+  user.save((err, userInfo) => {  //save하면 정보들이 userModel에 저장 됌
     if(err) return res.json({success: false, err})
     return res.status(200).json({
       success: true
     })
   })
+})
 
+app.post('/api/users/login', (req, res) => {
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user){
+      return res.json({
+        loginSuccess : false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    
+    //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err, isMatch ) => {
+      if(!isMatch)
+        return res.json({ loginSuccess: false, message : "비밀번호가 틀렸습니다."})
+        
+      //비밀번호까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
+        res.cookie ("x_auth", user.token) 
+        .status(200)
+        .json({loginSuccess: true, userId : user._id})
+      })
+    })
+  })
+})
+
+
+app.get('/api/users/auth', auth, (req, res) => {
+
+  //여기까지 미들웨어를 통과해 왔따는 얘기는 Authentication이 True라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth : true,
+    email: req.user.email,
+    name : req.user.name,
+    lastname : req.user.lastname,
+    role : req.user.role,
+    image : req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+  User.findOneAndUpdate({_id: req.user._id},
+  { token :"" }
+  ,(err, user) => {
+  if(err) return res.json({success: false, err});
+  return res.status(200).send({
+    success: true
+  })
+  })
 })
 
 app.listen(port, () => console.log(`Exangle app listening on prot ${port}!`))
